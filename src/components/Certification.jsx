bc524bebc92f8ca56daa1d8af50ac788277f4d15
yoshino <yoshino14710ous@gmail.com>
2023-03-04 20:35:28 +0900

    cerfication component

---
+++ src/components/Certification.jsx
@@ -0,0 +1,27 @@
+const Certification = () => {
+  return (
+    <>
+      <div
+        style={{
+          padding: "3rem",
+          fontWeight: 600,
+          fontSize: "1.5rem",
+          backgroundColor: "#ffeaee",
+          margin: "0 auto",
+          padding: "2rem 3rem",
+          lineHeight: "5rem"
+        }}
+      >
+        <h4 style={{ color: "#005689", textShadow: "-1px 1px #fff"}}>資格</h4>
+        <div>
+            <ul>
+                <li>TOEIC 900点 （2020/12）</li>
+            </ul>
+        </div>
+
+      </div>
+    </>
+  );
+};
+
+export { Certification };
