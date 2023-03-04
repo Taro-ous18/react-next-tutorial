221639097a084e66d2fbd7e534b90e0cba7b1189
yoshino <yoshino14710ous@gmail.com>
2023-03-04 18:07:23 +0900

    dev footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,33 @@
+const Footer = () => {
+  return (
+    <>
+      <div
+        style={{
+          width: "83%",
+          margin: "0 auto",
+          backgroundColor: "#272932",
+          display: "flex",
+          justifyContent: "space-between",
+          color: "#fff",
+          alignItems: "center"
+        }}
+      >
+        <h4 style={{ marginLeft: "3rem"}}>Taro Yoshino 2023</h4>
+        <div>
+          <ul
+            style={{ display: "flex", color: "white", listStyle: "none", marginRight: "1rem" }}
+            className="footer-sns"
+          >
+            <li style={{ marginRight: "1rem", }}>
+              1<img src="" alt="" />
+            </li>
+            <li>2</li>
+            <li>3</li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export { Footer };
